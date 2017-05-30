0cb2d1e6712ddf567a9d7175d6d3959cdc608b64
wamari <wamari@harlertechnologies.com>
2017-05-30 16:42:12 +0300

    Initial commit

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,25 @@
+public class  HelloWorld{
+    public static void main(String[] args) {
+      System.out.println("Java Rocks!");
+
+      // variables
+      int passengers; //declare variable
+      int stops;
+      String driver;
+      passengers = 0; //set variable value
+      passengers = passengers + 5; //assigning new value to variable
+      stops = 1;
+      passengers = passengers - 3;
+      stops = stops + 1;
+      passengers = passengers - 1 + 5;
+      stops = stops + 1;
+
+      driver = "Wamari"; //declare string variable
+      //count number of letters in the string
+      int letters = driver.length();
+
+      System.out.println("We have " + passengers + " passengers");
+      System.out.println("You have made " + stops + " stops!");
+      System.out.println(driver + " has " + letters +" in his name");
+    }
+  }
