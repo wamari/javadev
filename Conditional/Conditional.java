eab209eeeee0422a05f12e7182a30c0d7b29ff0e
wamari <wamari@harlertechnologies.com>
2017-06-02 07:52:30 +0300

    new samples conditional

--- Conditional/Conditional.java
+++ Conditional/Conditional.java
@@ -1,6 +1,6 @@
 public class Conditional{
   public static void main(String[] args) {
-    boolean isCold = true;
+    boolean isCold = true; //set to tru or false
     if(isCold){
       System.out.println("It is cold, wear a coat!");
     }
