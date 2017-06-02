aaa3759dc544b69ff7d77697020ae7dfe7774504
wamari <wamari@harlertechnologies.com>
2017-06-02 14:41:40 +0300

    functions intro

--- lesson3_functions/Lyrics.java
+++ lesson3_functions/Lyrics.java
@@ -1,5 +1,11 @@
 public class Lyrics{
   public static void main(String[] args) {
-    
+
+    System.out.println("This");
+    System.out.println("is");
+    System.out.println("my");
+    System.out.println("lyrics");
+    System.out.println("function");
+
   }
 }
