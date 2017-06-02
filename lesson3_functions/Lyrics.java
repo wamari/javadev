85327a61eec1c4e20fb4159acba7edb56f0d9e4b
wamari <wamari@harlertechnologies.com>
2017-06-02 14:54:04 +0300

    tweaks

--- lesson3_functions/Lyrics.java
+++ lesson3_functions/Lyrics.java
@@ -1,11 +1,12 @@
 public class Lyrics{
   public static void main(String[] args) {
-
-    System.out.println("This");
-    System.out.println("is");
-    System.out.println("my");
-    System.out.println("lyrics");
-    System.out.println("function");
-
+    public void chorus(){
+      System.out.println("This");
+      System.out.println("is");
+      System.out.println("my");
+      System.out.println("lyrics");
+      System.out.println("function");
+    }
+    chorus();
   }
 }
