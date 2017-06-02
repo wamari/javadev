41555822f2b8305cee60100944f887da20b81721
Eric Wamari <wamari@harlertechnologies.com>
2017-06-02 07:28:01 +0300

    added if statement

--- Conditional/Conditional.java
+++ Conditional/Conditional.java
@@ -1,5 +1,8 @@
 public class Conditional{
   public static void main(String[] args) {
-
+    boolean isCold = true;
+    if(isCold){
+      System.out.println("It is cold, wear a coat!");
+    }
   }
 }
