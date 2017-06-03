5c17f6a9fa49517db3672a75641dcbc015552bbb
Eric Wamari <wamari@harlertechnologies.com>
2017-06-03 18:17:25 +0300

    fixed lyrics file

--- lesson3_functions/Lyrics.java
+++ lesson3_functions/Lyrics.java
@@ -1,15 +1,12 @@
-//rework this program
-
-
 public class Lyrics{
-  //public static void main(String[] args) {
-    public staticvoid chorus(){
-      System.out.println("This");
-      System.out.println("is");
-      System.out.println("my");
-      System.out.println("lyrics");
-      System.out.println("function");
-    }
+  public static void chorus(){
+    System.out.println("This");
+    System.out.println("is");
+    System.out.println("my");
+    System.out.println("lyrics");
+    System.out.println("function");
+  }
+  public static void main(String[] args) {
     chorus();
-//  }
+  }
 }
